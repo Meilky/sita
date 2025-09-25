d67e4a9bb07b53fb2304d827b446579e260a292a
Meilky <mikaeljean2k3@gmail.com>
2025-09-25 15:04:33 -0400

    Render where char will be as the color of the char

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,15 @@
 use image::{ImageReader, Rgb, RgbImage};
 
 fn main() {
-    let img = ImageReader::open("hot_dog.png")
-        .unwrap()
-        .decode()
-        .unwrap();
+    let img = ImageReader::open("hot_dog.png").unwrap().decode().unwrap();
 
+    // mono space font
     let font = include_bytes!("../firacode_medium.ttf") as &[u8];
     let firacode = fontdue::Font::from_bytes(font, fontdue::FontSettings::default()).unwrap();
 
-    let (g_metric, _g_bitmap) = firacode.rasterize('g', 10.0);
+    let (g_metric, _g_bitmap) = firacode.rasterize('g', 20.0);
 
-    let char_height_u32: u32 = 10;
+    let char_height_u32: u32 = 20;
     let char_width_u32: u32 = u32::try_from(g_metric.width).unwrap();
 
     let buf = img.to_luma8();
@@ -22,22 +20,34 @@ fn main() {
     let nb_columns = width / char_width_u32;
     let nb_lines = height / char_height_u32;
 
-    println!("{}x{}", nb_columns, nb_lines);
-
     let mut img = RgbImage::new(width, height);
 
-    for (i, line) in buf
-        .chunks(usize::try_from(width).unwrap())
-        .into_iter()
-        .enumerate()
-    {
-        for (j, pixel) in line.iter().enumerate() {
-            img.put_pixel(
-                u32::try_from(j).unwrap(),
-                u32::try_from(i).unwrap(),
-                Rgb([*pixel, *pixel, *pixel]),
-            );
+    let mut char_color: Vec<u32> = vec![0; usize::try_from(nb_columns * nb_lines).unwrap()];
+
+    for (x, y, px) in buf.enumerate_pixels() {
+        if x >= char_width_u32*nb_columns || y >= char_height_u32*nb_lines {
+            continue;
         }
+
+        let char_x: usize = usize::try_from(x / char_width_u32).unwrap();
+        let char_y: usize = usize::try_from(y / char_height_u32).unwrap();
+
+        char_color[char_x + (char_y * usize::try_from(nb_columns).unwrap())] += u32::try_from(px.0[0]).unwrap();
+    }
+
+    for (x, y, _px) in buf.enumerate_pixels() {
+        if x >= char_width_u32*nb_columns || y >= char_height_u32*nb_lines {
+            continue;
+        }
+
+        let char_x: usize = usize::try_from(x / char_width_u32).unwrap();
+        let char_y: usize = usize::try_from(y / char_height_u32).unwrap();
+
+        let c = char_color[char_x + (char_y * usize::try_from(nb_columns).unwrap())];
+
+        let gradient = u8::try_from(c / (char_height_u32 * char_width_u32)).unwrap();
+
+        img.put_pixel(x, y, Rgb([gradient, gradient, gradient]));
     }
 
     img.save("hot_dog_ascii.png").unwrap();
